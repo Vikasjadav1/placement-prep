c4345761b018a718e7aef2549eb56004db2021aa
Vikasjadav <vikassen472@gmail.com>
2026-03-11 21:50:38 +0530

    11/3/26

---
+++ Q27.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter a number: ";
+    cin>>n;
+    int A=n;
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=i;j--){
+            cout<<A<<" ";
+           
+        }
+        A--;
+        cout<<endl;
+    }
+    return 0;
+}
