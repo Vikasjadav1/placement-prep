001366bc5251d320f674a003dc8f0d75d25ccc7d
Vikasjadav <vikassen472@gmail.com>
2026-03-13 21:57:08 +0530

    13/3/26

---
+++ Q35.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter a number: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+         cout<<"*"; 
+       
+
+       if(i>1){
+            for(int j=1;j<=2*i-3;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+
+         cout<<endl;
+
+    }
+
+     for(int i=n;i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+         cout<<"*"; 
+       
+
+      if(i>1){
+            for(int j=1;j<=2*i-3;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+
+         cout<<endl;
+
+    }
+  
+    return 0;
+}
